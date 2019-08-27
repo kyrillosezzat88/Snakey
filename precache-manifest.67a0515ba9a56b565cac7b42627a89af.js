80df04a593694532cb6ebd1ff099777ebfc8ce61
kyrillosezzat <kyrillosezzat300@gmail.com>
2019-08-27 02:22:07 -0700

    Updates

---
+++ precache-manifest.67a0515ba9a56b565cac7b42627a89af.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = [
+  {
+    "revision": "51d51d1c5a6dfbf84173966ca82048df",
+    "url": "/Snakey/static/media/about-img1.51d51d1c.png"
+  },
+  {
+    "revision": "8fc616b4bd9558e33137",
+    "url": "/Snakey/static/css/main.9309639a.chunk.css"
+  },
+  {
+    "revision": "384f648aa112ce20fe54",
+    "url": "/Snakey/static/js/runtime~main.28c92623.js"
+  },
+  {
+    "revision": "170cec940c7197c80f61",
+    "url": "/Snakey/static/js/2.823ec854.chunk.js"
+  },
+  {
+    "revision": "b8278949e57f84a5644b10f521687bd6",
+    "url": "/Snakey/static/media/hero-banner1.b8278949.png"
+  },
+  {
+    "revision": "ee34bc24e37cc9a4543ad2737608c6ee",
+    "url": "/Snakey/static/media/food1.ee34bc24.png"
+  },
+  {
+    "revision": "bffeee2471b66ba8d9d9feca5ab3f342",
+    "url": "/Snakey/static/media/hero-banner2.bffeee24.png"
+  },
+  {
+    "revision": "8fc616b4bd9558e33137",
+    "url": "/Snakey/static/js/main.d05f16e2.chunk.js"
+  },
+  {
+    "revision": "55bc2621527e3ba9da478a93ebbc92f3",
+    "url": "/Snakey/static/media/offer-img.55bc2621.png"
+  },
+  {
+    "revision": "92500c4315dd9b3854ed25ebf21b680a",
+    "url": "/Snakey/static/media/featured1.92500c43.png"
+  },
+  {
+    "revision": "b4d70ceb7d751a903eb3ab38f90708fb",
+    "url": "/Snakey/static/media/chef-2.b4d70ceb.png"
+  },
+  {
+    "revision": "760d4a0390dc89279aa89a26ca21338b",
+    "url": "/Snakey/static/media/chef-1.760d4a03.png"
+  },
+  {
+    "revision": "a4641ca3e6f08c83601fc23b2bf6c714",
+    "url": "/Snakey/static/media/reservation-bg.a4641ca3.png"
+  },
+  {
+    "revision": "8f54b0ade9b8ef9ca6809e0cecd0c77e",
+    "url": "/Snakey/static/media/cta-bg.8f54b0ad.png"
+  },
+  {
+    "revision": "d35609417e934a86bea6ae9ddfca6c06",
+    "url": "/Snakey/index.html"
+  }
+];
